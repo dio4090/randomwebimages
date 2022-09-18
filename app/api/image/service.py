51e2368051f43b3e7df7fa2488b02b854a86ac8d
Diogo Rocha <diogo.rocha42@outlook.com>
2022-09-17 21:36:23 -0300

    Ajuste

--- app/api/image/service.py
+++ app/api/image/service.py
@@ -37,7 +37,6 @@ class ImageService:
         # Verifica o número máximo de imagens
         #max_count = Image.query.filter(Image.id >= 1).count()
         max_count = 56
-        print("max_count: "+max_count)
 
         # Pega um random entre 1 e o número máximo de objetos de Imagem
         rid = random.randint(1, max_count)
